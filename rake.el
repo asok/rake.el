14c3b2694d4109f8b5695f22444ed216cbf407cb
Victor Deryagin <vderyagin@gmail.com>
2012-09-20 12:57:59 +0300

    added header

--- rake.el
+++ rake.el
@@ -1,3 +1,32 @@
+;;; rake.el --- rake task runner for GNU Emacs
+
+;; Copyright (C) 2012 Victor Deryagin
+
+;; Author: Victor Deryagin <vderyagin@gmail.com>
+;; Created: 16 Sep 2012
+;; Version: 0.0.1
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;;; Code:
+
 (eval-when-compile
   (require 'cl))
 
@@ -66,7 +95,6 @@
              (setq current-dir
                    (file-name-as-directory
                     (expand-file-name ".." current-dir))))
-
            (reached-filesystem-root-p ()
              (equal current-dir "/")))
       (loop
@@ -123,3 +151,5 @@
       (recenter-top-bottom))))
 
 (provide 'rake)
+
+;;; rake.el ends here
